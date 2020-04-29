90a54e6d470307f248c01cf37b34bf2eab39f58d
Sergio Di Martino <sergio@webdevserg.io>
2020-04-28 21:32:38 -0700

    grab input values, fix style

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+    $("button").on("click", function() {
+        event.preventDefault();
+        console.log("I've been clicked!");
+        let inputGenre = $(".inputGenre").val();
+        let inputYear = $(".inputYear").val();
+        let inputRating = $(".inputRating").val();
+        let inputCast = $(".inputCast").val().trim();
+        console.log(inputGenre);
+        console.log(inputYear);
+        console.log(inputRating);
+        console.log(inputCast);
+    });
 });
