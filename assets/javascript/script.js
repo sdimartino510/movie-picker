991189156821357efcf7696e5278036f354a2acc
Sergio Di Martino <sergio@webdevserg.io>
2020-05-02 22:41:36 -0700

    hides form on submit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,23 @@
-$(document).ready(function() {
-    $("button").on("click", function() {
-        event.preventDefault();
-        console.log("I've been clicked!");
-        let inputGenre = $(".inputGenre").val();
-        let inputYear = $(".inputYear").val();
-        let inputRating = $(".inputRating").val();
-        let inputCast = $(".inputCast").val().trim();
-        console.log(inputGenre);
-        console.log(inputYear);
-        console.log(inputRating);
-        console.log(inputCast);
-    });
+$(document).ready(function () {
+  const inputGenre = $(".inputGenre");
+  const inputYear = $(".inputYear");
+  const inputRating = $(".inputRating");
+  const inputCast = $(".inputCast");
+  const inputForm = $(".input-form");
+
+  $("button").on("click", function () {
+    event.preventDefault();
+
+    let genre = inputGenre.val();
+    let year = inputYear.val();
+    let rating = inputRating.val();
+    let cast = inputCast.val().trim();
+
+    console.log(genre);
+    console.log(year);
+    console.log(rating);
+    console.log(cast);
+
+    inputForm.css("display", "none");
+  });
 });
