e56a0a806e83fb3a71f950d1a650b7aa1e794dc8
Sergio Di Martino <sergio@webdevserg.io>
2020-05-06 13:31:31 -0700

    Changes api query to TMDB

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -22,13 +22,13 @@ $(document).ready(function () {
     $inputForm.css("display", "none");
     $resultsBox.css("display", "block");
 
-    const url = "http://www.omdbapi.com/?apikey=7978fb46&t='titanic'"
+    const url = "https://api.themoviedb.org/3/search/movie?api_key=0de9c836fc37aeab2f416c1622515802&query=Jack+Reacher"
 
     $.ajax({
         url,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+        console.log(response.results[0]);
     });
   });
 });
